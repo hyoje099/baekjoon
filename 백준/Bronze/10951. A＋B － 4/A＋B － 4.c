4ee0b5c96994e544d27a9490bc8f123cd093c0bc
hyoje099 <109017711+hyoje099@users.noreply.github.com>
2023-02-19 12:39:37 +0900

    Update A＋B － 4.c

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.c
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.c
@@ -6,3 +6,5 @@ int main(void) {
         printf("%d\n", a+b);
     }
 }
+/*EOF는 End Of File의 약자로 파일의 끝을 표현하기 위해 -1로 정의된 상수입니다.
+즉, scanf("%d %d", &a, &b)!=EOF의 의미는 파일의 끝에 도달할 때까지 while문을 실행한다는 의미입니다.*/
